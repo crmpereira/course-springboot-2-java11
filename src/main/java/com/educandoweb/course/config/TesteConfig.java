987c47594832a90b54d89924f13e53dceb7b5f61
Cesar.Pereira <cesar.pereiram@gmail.com>
2020-07-09 21:06:46 -0300

    JPA repository, Corre;óes

--- src/main/java/com/educandoweb/course/config/TesteConfig.java
+++ src/main/java/com/educandoweb/course/config/TesteConfig.java
@@ -11,7 +11,7 @@ import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
 
 @Configuration
-@Profile("teste")
+@Profile("test")
 public class TesteConfig implements CommandLineRunner{
 
 	@Autowired
