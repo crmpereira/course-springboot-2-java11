c8e0dfb3ed6fc95c40383b822dace414f3768a0c
Cesar.Pereira <cesar.pereiram@gmail.com>
2020-07-12 10:21:38 -0300

    Insert User  POST

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -25,9 +25,16 @@ public class UserService {
 	public User findById(Long id) {
 	
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
-	
+		return obj.get();	
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+		
+	}
+	
+	
+	
 	
 	
 }
